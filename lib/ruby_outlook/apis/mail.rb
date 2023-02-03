a84126f728206bbbf73f9e7acaf3e1f2e3b73acd
Jeremiah Church <jeremiahchurch@gmail.com>
2023-02-03 13:39:44 -0500

    add folder delta methods for mail

--- lib/ruby_outlook/apis/mail.rb
+++ lib/ruby_outlook/apis/mail.rb
@@ -32,6 +32,7 @@ module RubyOutlook
       JSON.parse(response)
     end
 
+    # https://learn.microsoft.com/en-us/graph/api/message-delta?view=graph-rest-1.0&tabs=http
     def synchonize_messages_for_folder(folder_id, **args)
       request_url = "/#{user_or_me(args[:user])}/MailFolders/#{folder_id}/messages/delta"
       request_params = build_request_params(args)
@@ -42,6 +43,48 @@ module RubyOutlook
 
       response = make_api_call(:get, request_url, request_params, headers)
       JSON.parse(response)
+    end
+
+    # https://learn.microsoft.com/en-us/graph/api/mailfolder-delta?view=graph-rest-1.0&tabs=http
+    def synchonize_mail_folders(**args)
+      request_url = "/#{user_or_me(args[:user])}/MailFolders/delta"
+      request_params = build_request_params(args)
+
+      headers = {
+        'Prefer' => ['odata.track-changes', "odata.maxpagesize=#{args[:max_page_size].presence || 50}"]
+      }
+
+      response = make_api_call(:get, request_url, request_params, headers)
+      JSON.parse(response)
+    end
+
+    # https://learn.microsoft.com/en-us/graph/api/user-list-mailfolders?view=graph-rest-1.0&tabs=http
+    def get_folders(**args)
+      request_url = "/#{user_or_me(args[:user])}/mailFolders"
+      request_params = build_request_params(args)
+
+      headers = {
+        'Prefer' => ['odata.track-changes', "odata.maxpagesize=#{args[:max_page_size].presence || 50}"]
+      }
+
+      response = make_api_call(:get, request_url, request_params, headers)
+      JSON.parse(response) if response.present?
+    end
+
+    # https://learn.microsoft.com/en-us/graph/api/mailfolder-get?view=graph-rest-1.0&tabs=http
+    def get_folder_by_id(folder_id, _fields = nil, user = nil)
+      request_url = "/#{user_or_me(user)}/mailFolders/#{folder_id}"
+
+      response = make_api_call(:get, request_url)
+      JSON.parse(response) if response.present?
+    end
+
+    # https://learn.microsoft.com/en-us/graph/api/mailfolder-list-childfolders?view=graph-rest-1.0&tabs=http
+    def get_folder_children_by_id(folder_id, _fields = nil, user = nil)
+      request_url = "/#{user_or_me(user)}/mailFolders/#{folder_id}/childFolders"
+
+      response = make_api_call(:get, request_url)
+      JSON.parse(response) if response.present?
     end
 
     def get_message_by_id(id, fields = nil, user = nil)
@@ -111,13 +154,6 @@ module RubyOutlook
       request_url = "/#{user_or_me(user)}/messages/#{message_id}/send"
 
       response = make_api_call(:post, request_url)
-      JSON.parse(response) if response.present?
-    end
-
-    def get_folders(user: nil)
-      request_url = "/#{user_or_me(user)}/mailFolders"
-
-      response = make_api_call(:get, request_url)
       JSON.parse(response) if response.present?
     end
 
